02126cd2ff007ab2dde3d030ffec06204c86d85b
daara0322 <vidmin74@gmail.com>
2026-07-02 15:12:22 +0900

    [Passed] 100점 도달하기 (50ms, 16MB)

---
+++ trail0/100점 도달하기/reach-100-point.py
@@ -0,0 +1,14 @@
+number = int(input())
+
+for num in range(number, 101) :
+    if num >= 90 :
+        print("A", end=" ")
+    elif num >= 80:
+        print("B", end =" ")
+    elif num >= 70:
+        print("C", end =" ")
+    elif num >= 60:
+        print("D", end =" ")
+    else :
+        print("F", end =" ")
+    
