0aa3ae8772f8b11c7f96694c0b502446c269d94a
daara0322 <vidmin74@gmail.com>
2026-07-02 17:02:11 +0900

    [Passed] A 부터 B 까지 (52ms, 16MB)

---
+++ trail0/A 부터 B 까지/a-to-b.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+
+while True :
+    if a <= b :
+        print(a, end=" ")
+        if a % 2 == 1:
+            a *= 2
+        elif a % 2 == 0:
+            a += 3
+    else : 
+        break
